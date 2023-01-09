310433dafa38e45bc7b79b3ba0126f752138fcbe
Amir_progg <75519329+Am1rTl@users.noreply.github.com>
2023-01-09 18:43:26 +0300

    Add files via upload

--- brute.py
+++ brute.py
@@ -1,6 +1,7 @@
 a = "qwertyuiop[]asdfghjkl;'zxcvbnm,./QWERTYUIOP{}|ASDFGHJKL:ZXCVBNM<>?1234567890!@#$%^&*()-=_+`~ " + '"' 
 mass = []
 chore = 0
+max_char = int(input("Write the maximum number of characters: "))
 def brute(a,st,pl,numb):
 	global chore
 	st += pl
@@ -8,13 +9,13 @@ def brute(a,st,pl,numb):
 		for i in a:
 			brute(a,st,i,numb)
 	else:
-		#print(st)
+		print(st)
 		chore +=1
 
 for i in range(len(a)):
 	mass.append(a[i])
 
-for i in range(2):
+for i in range(max_char+1):
 	brute(a,'','',i)
 print("Altogether:",chore)
 
